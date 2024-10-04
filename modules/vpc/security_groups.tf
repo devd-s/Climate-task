f6809d7d1f2957ed35438afa5cd1efff3d65b8b8
Davesh <65277282+devd-s@users.noreply.github.com>
2024-10-04 09:14:21 +0200

    Update security_groups.tf

--- modules/vpc/security_groups.tf
+++ modules/vpc/security_groups.tf
@@ -4,12 +4,12 @@
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
 
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name        = "${var.environment}-ecs-sg"
